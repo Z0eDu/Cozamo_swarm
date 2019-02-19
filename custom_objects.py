2c83bde4ddee79a97f5ddeb1dbc838ee60e62668
evankravitz <esk95@cornell.edu>
2019-02-19 16:35:57 -0500

    new files

---
+++ custom_objects.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''This example demonstrates how you can define custom objects.
+
+The example defines several custom objects (2 cubes, a wall and a box). When
+Cozmo sees the markers for those objects he will report that he observed an
+object of that size and shape there.
+
+You can adjust the markers, marker sizes, and object sizes to fit whatever
+object you have and the exact size of the markers that you print out.
+'''
+
+import time
+
+import cozmo
+from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
+
+
+def handle_object_appeared(evt, **kw):
+    # This will be called whenever an EvtObjectAppeared is dispatched -
+    # whenever an Object comes into view.
+    if isinstance(evt.obj, CustomObject):
+        print("Cozmo started seeing a %s" % str(evt.obj.object_type))
+
+
+def handle_object_disappeared(evt, **kw):
+    # This will be called whenever an EvtObjectDisappeared is dispatched -
+    # whenever an Object goes out of view.
+    if isinstance(evt.obj, CustomObject):
+        print("Cozmo stopped seeing a %s" % str(evt.obj.object_type))
+
+
+def custom_objects(robot: cozmo.robot.Robot):
+    # Add event handlers for whenever Cozmo sees a new object
+    robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+    robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
+
+
+
+
+
+    custom_box = robot.world.define_custom_box(custom_object_type=cozmo.objects.CustomObjectTypes.CustomType00, \
+                                                       marker_front=cozmo.objects.CustomObjectMarkers.Circles2, \
+                                                       marker_back=cozmo.objects.CustomObjectMarkers.Circles3, \
+                                                       marker_top=cozmo.objects.CustomObjectMarkers.Circles4, \
+                                                       marker_bottom=cozmo.objects.CustomObjectMarkers.Circles5, \
+                                                       marker_left=cozmo.objects.CustomObjectMarkers.Diamonds2, \
+                                                       marker_right=cozmo.objects.CustomObjectMarkers.Diamonds3, \
+                                                       depth_mm=60, \
+                                                       width_mm=60, \
+                                                       height_mm=45, \
+                                                       marker_width_mm=24.892, \
+                                                       marker_height_mm=24.892, \
+                                                       is_unique=True)
+
+    if (custom_box is not None):
+        print("All objects defined successfully!")
+    else:
+        print("One or more object definitions failed!")
+        return
+
+    print("Show the above markers to Cozmo and you will see the related objects "
+          "annotated in Cozmo's view window, you will also see print messages "
+          "everytime a custom object enters or exits Cozmo's view.")
+
+    print("Press CTRL-C to quit")
+
+
+
+    lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+    custom_boxes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.CustomObjectTypes.CustomType00, timeout=60)
+    lookaround.stop()
+    if len(custom_boxes) > 0:
+    	robot.go_to_object(target_object = custom_boxes[0], distance_from_object = 100, num_retries = 5)
+        # robot.drive_wheels(25, 25)
+        # time.sleep(1)
+        # robot.set_lift_height(0.5, accel=10.0, max_speed=10.0, duration=0.0,  in_parallel=False, num_retries=1)
+        # robot.drive_wheels(25, 25)
+        # time.sleep(1)
+        # robot.set_lift_height(1, accel=10.0, max_speed=10.0, duration=0.0,  in_parallel=False, num_retries=1)
+        # robot.drive_wheels(-25, -25)
+    else:
+        print("Cannot locate custom box")
+
+
+    while True:
+        time.sleep(0.1)
+
+
+cozmo.run_program(custom_objects, use_viewer=True)
